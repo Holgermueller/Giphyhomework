04ce1bb8e2ca9afb150b2500294a3f0de2e8a070
Holger Mueller <9jholger@gmail.com>
2018-03-22 15:21:31 -0500

    attached reset css and some js work

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,6 +1,7 @@
 'use strict';
 
 $(document).ready(function(){
+
 //variables to hold topics and other things
 
 
@@ -9,26 +10,32 @@ $(document).ready(function(){
                         'turtles',
                         'sloths',
                         'frogs'];
+                        //console.log(topics);
+
+    $("#addAnimal").on("click",function() {
 
             let searchField = $("#animalInput").val()
-    //console.log(searchField);
+    
 
     //POWERED BY GIPHY
-
     // API KEY : kt3AVxl1bzJdKflIKnVDdxqLJZS6gVAQ
 
-            let queryURL = "https://api.giphy.com/v1/gifs/search?q=geckos&api_key=kt3AVxl1bzJdKflIKnVDdxqLJZS6gVAQ";
+    //get search box to work
+
+            let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchField + "&api_key=kt3AVxl1bzJdKflIKnVDdxqLJZS6gVAQ";
             
             $.ajax({
                 url:queryURL,
                 method: "GET"
             }).then(function(response){
                 console.log(response);
-            })
+
+
+            });
 
 
 
 
-
+    });
 
 });
