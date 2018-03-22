a5a9f3cc8047d6da2766e8d413f2e9a5195b4e60
Holger Mueller <9jholger@gmail.com>
2018-03-22 13:52:12 -0500

    some css and js

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,8 +1,34 @@
 'use strict';
 
-//variable to hold topics
+$(document).ready(function(){
+//variables to hold topics and other things
 
 
 
-// API KEY : kt3AVxl1bzJdKflIKnVDdxqLJZS6gVAQ
+            let topics = ['geckos',
+                        'turtles',
+                        'sloths',
+                        'frogs'];
 
+            let searchField = $("#animalInput").val()
+    //console.log(searchField);
+
+    //POWERED BY GIPHY
+
+    // API KEY : kt3AVxl1bzJdKflIKnVDdxqLJZS6gVAQ
+
+            let queryURL = "https://api.giphy.com/v1/gifs/search?q=geckos&api_key=kt3AVxl1bzJdKflIKnVDdxqLJZS6gVAQ";
+            
+            $.ajax({
+                url:queryURL,
+                method: "GET"
+            }).then(function(response){
+                console.log(response);
+            })
+
+
+
+
+
+
+});
