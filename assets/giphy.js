901ae1610d1fc4e6a1767521a69f831899e2e55f
Holger Mueller <9jholger@gmail.com>
2018-03-24 09:31:20 -0500

    got array buttons on DOM

--- assets/giphy.js
+++ assets/giphy.js
@@ -4,13 +4,24 @@ $(document).ready(function(){
 
 //initial array of topics
 
-
-
     let topics = ['geckos',
                 'turtles',
                 'sloths',
-                'frogs',
-                ''];
+                'frogs'
+                ];
+
+//append buttons for array
+
+        for (let i = 0; i < topics.length; i++) {
+            let animalButton = $('<button>');
+            animalButton.addClass("animal-button");
+            animalButton.attr("data-name", topics[i]);
+            animalButton.text(topics[i]);
+            $("#animalButtons").append(animalButton);
+
+//create new buttons when animal name is typed into search field
+
+
 
     $("#addAnimal").on("click", function(e) {
         e.preventDefault() //use this to prevent some form default functions
@@ -20,49 +31,26 @@ $(document).ready(function(){
     //querrying API and getting search field to work
 
             let topic = $("#animalInput").val()
-            //console.log(topic);
+            console.log(topic);
             let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=kt3AVxl1bzJdKflIKnVDdxqLJZS6gVAQ";
             
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).then(function(response) {
-                console.log(response);
+                //console.log(response);
                 for ( let j = 0 ; j < response.data.length ; j++) {
                     const animalTemplate = `<div class="pen">
                     <p class="rating"> Rating: ${response.data[j].rating}</p>
                     <img src="${response.data[j].images.original.url}" class="jpeg">  </img>
                     </div>`;
-
+                    //create an element to display the rating (above code)
+                    //append the GIF to DOM
                     $("#animals").append(animalTemplate);
                 }
 
-//append the GIF to DOM
-
-
-
+                 });
 //add stop/start limit to GIFs play
-
-
-
-//create an element to display the rating
-
-
-
             });
-//append buttons
-  /*      function renderButtons() {
-           $("#animalButtons").empty();
-           for (let i = 0; i < topics.lengt; i++) {
-               let button = $("<button>");
-               button.addClass("animal-button");
-               button.attr("data-name", topics[i]);
-               button.text(topics[i]);
-               $("#animalButtons").append(button);
-           }
-        } */
-
-
-    });
-
+    }
 });
