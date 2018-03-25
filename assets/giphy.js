236fa0ff1a8c7bf5cd79ba9e15365f71ff9f1e13
Holger Mueller <9jholger@gmail.com>
2018-03-25 13:48:39 -0500

    fixed some things

--- assets/giphy.js
+++ assets/giphy.js
@@ -7,24 +7,32 @@ $(document).ready(function(){
     let topics = ['geckos',
                 'turtles',
                 'sloths',
-                'frogs'
-                ];
-
+                'frogs'];
 //append buttons for array
 
-        for (let i = 0; i < topics.length; i++) {
-            let animalButton = $('<button>');
-            animalButton.addClass("animal-button");
-            animalButton.attr("data-name", topics[i]);
-            animalButton.text(topics[i]);
-            $("#animalButtons").append(animalButton);
-
-//create new buttons when animal name is typed into search field
-
+    for (let i = 0; i < topics.length; i++) {
+        const animalBtns = $("<button>");
+            animalBtns.addClass("animal-btn");
+            animalBtns.attr("anml-name", topics[i]);
+            animalBtns.text(topics[i]);
+            $("#animalButtons").append(animalBtns);
+    }
 
 
     $("#addAnimal").on("click", function(e) {
         e.preventDefault() //use this to prevent some form default functions
+        $("#animals").empty();
+        //use search field to append more buttons
+        let newBtn = $("animalInput").val();
+                newBtn.addClass("animal-btn");
+                newBtn.attr("anml-name");
+                newBtn.text("#animals");
+                $("#animalButtons").append(newBtn);
+
+        });
+
+        
+
     //POWERED BY GIPHY
     // API KEY : kt3AVxl1bzJdKflIKnVDdxqLJZS6gVAQ
 
@@ -44,13 +52,19 @@ $(document).ready(function(){
                     <p class="rating"> Rating: ${response.data[j].rating}</p>
                     <img src="${response.data[j].images.original.url}" class="jpeg">  </img>
                     </div>`;
+
                     //create an element to display the rating (above code)
                     //append the GIF to DOM
+                    
                     $("#animals").append(animalTemplate);
+                    
+                    //append button new button
+
+                    
+                
                 }
 
                  });
 //add stop/start limit to GIFs play
-            });
-    }
 });
+    
