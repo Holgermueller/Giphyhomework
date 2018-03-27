f1a0331ebc4cde086f5bb2eefc7077e52bd47027
Holger Mueller <9jholger@gmail.com>
2018-03-26 20:47:51 -0500

    got initial buttons to work, sort of

--- assets/giphy.js
+++ assets/giphy.js
@@ -8,63 +8,66 @@ $(document).ready(function(){
                 'turtles',
                 'sloths',
                 'frogs'];
-//append buttons for array
+
+    //append buttons for array
 
     for (let i = 0; i < topics.length; i++) {
         const animalBtns = $("<button>");
             animalBtns.addClass("animal-btn");
             animalBtns.attr("anml-name", topics[i]);
             animalBtns.text(topics[i]);
+            animalBtns.val(topics[i]);
             $("#animalButtons").append(animalBtns);
     }
 
+    //POWERED BY GIPHY
+    // API KEY : kt3AVxl1bzJdKflIKnVDdxqLJZS6gV
+    //querrying API and getting search field to work
+
+        $("#animalButtons").on("click",function(e) {
+            e.preventDefault()
+            $("#animals").empty();
+
+                let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=kt3AVxl1bzJdKflIKnVDdxqLJZS6gVAQ";
+
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }).then(function(response) {
+                    //console.log(response);
+                    for ( let j = 0 ; j < response.data.length ; j++) {
+                        const animalTemplate = `<div class="pen">
+                        <p class="rating"> Rating: ${response.data[j].rating}</p>
+                        <img src="${response.data[j].images.original.url}" class="jpeg">  </img>
+                        </div>`;
+                        //create an element to display the rating (above code)
+                        //append the GIF to DOM                    
+                        $("#animals").append(animalTemplate);
+                        }    
+                })
+        });
+
+    
+
+    //append a new button when a new animal is searched
 
     $("#addAnimal").on("click", function(e) {
         e.preventDefault() //use this to prevent some form default functions
         $("#animals").empty();
         //use search field to append more buttons
         let newBtn = $("animalInput").val();
-                newBtn.addClass("animal-btn");
+                newBtn
                 newBtn.attr("anml-name");
                 newBtn.text("#animals");
+                newBtn.val(topics[i]);
                 $("#animalButtons").append(newBtn);
+    });
 
-        });
 
-        
-
-    //POWERED BY GIPHY
-    // API KEY : kt3AVxl1bzJdKflIKnVDdxqLJZS6gVAQ
-
-    //querrying API and getting search field to work
-
-            let topic = $("#animalInput").val()
-            console.log(topic);
-            let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=kt3AVxl1bzJdKflIKnVDdxqLJZS6gVAQ";
-            
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function(response) {
-                //console.log(response);
-                for ( let j = 0 ; j < response.data.length ; j++) {
-                    const animalTemplate = `<div class="pen">
-                    <p class="rating"> Rating: ${response.data[j].rating}</p>
-                    <img src="${response.data[j].images.original.url}" class="jpeg">  </img>
-                    </div>`;
-
-                    //create an element to display the rating (above code)
-                    //append the GIF to DOM
-                    
-                    $("#animals").append(animalTemplate);
-                    
-                    //append button new button
-
-                    
-                
-                }
-
-                 });
 //add stop/start limit to GIFs play
+
+
+
+
 });
     
