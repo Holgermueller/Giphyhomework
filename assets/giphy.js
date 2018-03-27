73b201cf795b6f1e546aa71d5bd29c4478af3c53
Holger Mueller <9jholger@gmail.com>
2018-03-27 12:28:23 -0500

    buttons append but not apt GIFs

--- assets/giphy.js
+++ assets/giphy.js
@@ -9,46 +9,18 @@ $(document).ready(function(){
                 'sloths',
                 'frogs'];
 
-    //append buttons for array
+        //append buttons for array
 
-function makeBtns() {
-
-    for (let i = 0; i < topics.length; i++) {
-        const animalBtns = $("<button>");
-            animalBtns.addClass("animal-btn");
-            animalBtns.attr("anml-name");
-            animalBtns.text(topics[i]);
-            animalBtns.val(topics[i]);
-            $("#animalButtons").append(animalBtns);
-    }
-}
-
-    //POWERED BY GIPHY
-    // API KEY : kt3AVxl1bzJdKflIKnVDdxqLJZS6gV
-    //querrying API and getting search field to work
-
-        $("#animalButtons").on("click",function(e) {
-            e.preventDefault()
-            $("#animals").empty();
-
-            let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=kt3AVxl1bzJdKflIKnVDdxqLJZS6gVAQ";
-
-                $.ajax({
-                    url: queryURL,
-                    method: "GET"
-                }).then(function(response) {
-                    //console.log(response);
-                    for ( let j = 0 ; j < response.data.length ; j++) {
-                        const animalTemplate = `<div class="pen">
-                        <p class="rating"> Rating: ${response.data[j].rating}</p>
-                        <img src="${response.data[j].images.original.url}" class="jpeg">  </img>
-                        </div>`;
-                        //create an element to display the rating (above code)
-                        //append the GIF to DOM                    
-                        $("#animals").append(animalTemplate);
-                        }    
-                })
-        });
+        function makeBtns(){
+            for (let i = 0; i < topics.length; i++) {
+                const animalBtns = $("<button>");
+                    animalBtns.addClass("animal-btn");
+                    animalBtns.attr("anml-name");
+                    animalBtns.text(topics[i]);
+                    animalBtns.val(topics[i]);
+                    $("#animalButtons").append(animalBtns);
+            }
+        }
 
     //append a new button when a new animal is searched
 
@@ -59,7 +31,42 @@ function makeBtns() {
             console.log(topic);
             topics.push(topic);
             makeBtns();
+
+
+    //POWERED BY GIPHY
+    // API KEY : kt3AVxl1bzJdKflIKnVDdxqLJZS6gV
+    //querrying API and getting search field to pull up GIFs from array
+
+        $("#animalButtons").on("click",function(e) {
+            e.preventDefault()
+            $("#animals").empty();
+
+            let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=kt3AVxl1bzJdKflIKnVDdxqLJZS6gVAQ";
+
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }).then(function(response) {
+                    //console.log(response);
+                    //create DIV to append rating
+                    //create DIV to append GIFs
+                    $("#animals").empty();
+                    for ( let j = 0 ; j < response.data.length ; j++) {
+                        const animalTemplate = `<div class="pen">
+                        <p class="rating"> Rating: ${response.data[j].rating}</p>
+                        <img src="${response.data[j].images.original.url}" class="jpeg">  </img>
+                        </div>`;
+                        //append the GIF to DOM            
+                        $("#animals").append(animalTemplate);
+                        console.log(animalTemplate);
+                    }    
+                })
+        });
+
     });
+
+
+
 
 
 //add stop/start limit to GIFs play
