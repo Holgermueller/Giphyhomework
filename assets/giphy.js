0b182e3e385ac02dc2bb0b982e6a26096a6960d9
Holger Mueller <9jholger@gmail.com>
2018-03-27 22:35:18 -0500

    it works

--- assets/giphy.js
+++ assets/giphy.js
@@ -2,25 +2,26 @@
 
 $(document).ready(function(){
 
-//initial array of topics
-
+    //initial array of topics
     let topics = ['geckos',
                 'turtles',
                 'sloths',
                 'frogs'];
 
-        //append buttons for array
+    //append buttons for array
 
-        function makeBtns(){
-            for (let i = 0; i < topics.length; i++) {
-                const animalBtns = $("<button>");
-                    animalBtns.addClass("animal-btn");
-                    animalBtns.attr("anml-name", topics[i]);
-                    animalBtns.text(topics[i]);
-                    animalBtns.val(topics[i]);
-                    $("#animalButtons").append(animalBtns);
-            }        
-        }
+    function makeBtns(){
+        for (let i = 0; i < topics.length; i++) {
+            const animalBtns = $("<button>");
+                animalBtns.addClass("animal-btn");
+                animalBtns.attr("anml-name", topics[i]);
+                animalBtns.text(topics[i]);
+                animalBtns.val(topics[i]);
+                $("#animalButtons").append(animalBtns);
+        }        
+    }
+    makeBtns();
+
     //append a new button when a new animal is searched
 
     $("#addAnimal").on("click", function(e) {
@@ -30,39 +31,46 @@ $(document).ready(function(){
             console.log(topic);
             topics.push(topic);
             makeBtns();
-
+        });
 
     //POWERED BY GIPHY
     // API KEY : kt3AVxl1bzJdKflIKnVDdxqLJZS6gV
     //querrying API and getting search field to pull up GIFs from array
 
-        $("#animalButtons").on("click",function(e) {
-            e.preventDefault()
-            $("#animals").empty();
+    $(document).on("click", '.animal-btn', function(e) {
+        e.preventDefault()
+        $("#animals").empty();
 
-            let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=kt3AVxl1bzJdKflIKnVDdxqLJZS6gVAQ";
+        // when button is created, store the topic on it somehow (.data())
+        let data = ($(this).attr("anml-name"));
 
-                $.ajax({
-                    url: queryURL,
-                    method: "GET"
-                }).then(function(response) {
-                    //console.log(response);
-                    //create DIV to append rating
-                    //create DIV to append GIFs
-                    $("#animals").empty();
-                    for ( let j = 0 ; j < response.data.length ; j++) {
-                        const animalTemplate = `<div class="pen">
-                        <p class="rating"> Rating: ${response.data[j].rating}</p>
-                        <img src="${response.data[j].images.original.url}" class="jpeg">  </img>
-                        </div>`;
-                        //append the GIF to DOM            
-                        $("#animals").append(animalTemplate);
-                        console.log(animalTemplate);
-                    }    
-                })
-        });
+        // retrieve that topic and store it in a variable called topic
 
+        let topic = data;
+
+        let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=kt3AVxl1bzJdKflIKnVDdxqLJZS6gVAQ";
+        //console.log(topic);
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function(response) {
+                //console.log(response);
+                //create DIV to append rating
+                //create DIV to append GIFs
+                $("#animals").empty();
+                for ( let j = 0 ; j < response.data.length ; j++) {
+                    const animalTemplate = `<div class="pen">
+                    <p class="rating"> Rating: ${response.data[j].rating}</p>
+                    <img src="${response.data[j].images.original.url}" class="jpeg">  </img>
+                    </div>`;
+                    //append the GIF to DOM            
+                    $("#animals").append(animalTemplate);
+                    //console.log(animalTemplate);
+                }    
+            })
     });
+
+
 
 
 //add stop/start limit to GIFs play
