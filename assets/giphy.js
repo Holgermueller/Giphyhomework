38421569bbe1e83af6a6cb71f8329a9c4435f5e9
Holger Mueller <9jholger@gmail.com>
2018-12-27 13:11:27 -0500

    got rating into local storage

--- assets/giphy.js
+++ assets/giphy.js
@@ -41,18 +41,19 @@ $(document).ready(() => {
             }).then(response => {
                 $("#animals").empty();
                 $.map(response.data, gif => {
+                    let gifRating = gif.rating;
                     let gifID = gif.id;
                     let gridItem = $('<div>').addClass("grid-item");
                     let imgDIV = $("<div>").addClass("singleGif");
-                    let rating = $(`<p class="rating">Rating: ${(gif.rating).toUpperCase()}</p>`);
+                    let rating = $(`<p class="rating">Rating: ${(gifRating).toUpperCase()}</p>`);
                     let gifIMG = $("<img>").addClass("jpeg")
                         .attr('src', gif.images.downsized_still.url)
                         .attr('data-jpeg_src', gif.images.downsized_medium.url);
-                        // .attr('data-index', gifID);
                     let buttonContainer = $("<div>").addClass("button-container");
                     let downloadLink = $('<a download>').attr('target', '_blank').attr("href", gif.images.original.url);
                     let downloadButton = $('<button>').addClass('download-button').text("Download");
-                    let favoriteButton = $("<input type='submit' value='Favorite'>").addClass("favorite-button").attr("id", "favorite").attr('data-index', gifID);
+                    let favoriteButton = $("<input type='submit' value='Favorite'>").addClass("favorite-button").attr("id", "favorite").attr('data-index', gifID)
+                    .attr('data-rating', gifRating);
 
                     $("#animals").append(gridItem);
                     gridItem.append(imgDIV);
@@ -65,7 +66,12 @@ $(document).ready(() => {
                 let faveGifs = JSON.parse(localStorage.getItem('faveGifs')) || [];
                 $(document).on('click', '#favorite', function() {
                     console.log(faveGifs);
-                    let faveGif = $(this).data('index');
+                    let id = $(this).data('index');
+                    let rating = $(this).data('rating');
+                    let faveGif = {
+                        id,
+                        rating
+                    };
                     faveGifs.push(faveGif);
                     localStorage.setItem('faveGifs', JSON.stringify(faveGifs));
                     populateFaves();
@@ -74,22 +80,31 @@ $(document).ready(() => {
     });
 
     let faveGifRating = JSON.parse(localStorage.getItem('faveGifs'));
-    console.log(faveGifRating);
 
     function populateFaves() {
         $.map(faveGifRating, faveGif => {
+            let rating = faveGif.rating;
             let gridItem = $('<div>').addClass("grid-item");
             let faveDiv = $("<div>").addClass("fave-gif");
-            let faveRating = $("<p class='rating'>Rating: " + faveGifRating + "</p>");
+            let faveRating = $(`<p class='rating'>Rating: ${(rating).toUpperCase()}</p>`);
             let removeButton = $("<button>").addClass("remove-button").attr("id", "remove").text("Remove");
 
             $("#favoriteGifs").append(gridItem);
             gridItem.append(faveDiv);
+            faveDiv.append(faveRating);
             faveDiv.append(faveGif);
             faveDiv.append(removeButton);
         });
     }
     populateFaves();
+
+    function removeAFavorite() {
+        $(document).on('click', '#remove', () => {
+            console.log("click");
+            localStorage.removeItem('faveGifs');
+        });
+    }
+    removeAFavorite();
 
     $(document).on('mouseenter', '.jpeg', function() {
         $(this).data('img_src', $(this).attr('src'));
@@ -99,11 +114,5 @@ $(document).ready(() => {
     $(document).on('mouseleave', '.jpeg', function() {
         $(this).attr('src', $(this).data('img_src'));
     });
-
-    $(document).on('click', '#remove', () => {
-        console.log("click");
-        localStorage.removeItem('faveGifs');
-    });
-
 });
     
