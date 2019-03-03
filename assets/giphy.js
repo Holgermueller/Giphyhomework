680d8a8e35b6033ae09f48652d8c8f3fea28363a
Holger Mueller <9jholger@gmail.com>
2019-03-03 14:08:19 -0600

    mapped over localstorage

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,11 +1,7 @@
-'use strict';
+"use strict";
 
 $(document).ready(() => {
-  let topics = ['geckos',
-    'turtles',
-    'sloths',
-    'frogs',
-    'rabbits'];
+  let topics = ["geckos", "turtles", "sloths", "frogs", "rabbits"];
 
   function makeBtns() {
     $.map(topics, topic => {
@@ -22,18 +18,23 @@ $(document).ready(() => {
   $("#addAnimal").on("click", e => {
     e.preventDefault();
     $("#animalButtons").empty();
-    let topic = $("#animalInput").val().trim();
+    let topic = $("#animalInput")
+      .val()
+      .trim();
     topics.push(topic);
     makeBtns();
   });
 
   //POWERED BY GIPHY
-  $(document).on("click", '.animal-btn', function (e) {
-    e.preventDefault()
+  $(document).on("click", ".animal-btn", function(e) {
+    e.preventDefault();
     $("#animals").empty();
-    let data = ($(this).attr("anml-name"));
+    let data = $(this).attr("anml-name");
     let topic = data;
-    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=kt3AVxl1bzJdKflIKnVDdxqLJZS6gVAQ";
+    let queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      topic +
+      "&api_key=kt3AVxl1bzJdKflIKnVDdxqLJZS6gVAQ";
     $.ajax({
       url: queryURL,
       method: "GET"
@@ -45,34 +46,47 @@ $(document).ready(() => {
         let stillGif = gif.images.downsized_still.url;
         let movingGif = gif.images.downsized_medium.url;
 
-        let gridItem = $('<div>').addClass("grid-item");
+        let gridItem = $("<div>").addClass("grid-item");
         let imgDIV = $("<div>").addClass("single-gif");
-        let rating = $(`<p class="rating">Rating: ${(gifRating).toUpperCase()}</p>`);
-        let gifIMG = $("<img alt='img'>").addClass("jpeg")
-          .attr('src', stillGif)
-          .attr('data-jpeg_src', movingGif);
+        let rating = $(
+          `<p class="rating">Rating: ${gifRating.toUpperCase()}</p>`
+        );
+        let gifIMG = $("<img alt='img'>")
+          .addClass("jpeg")
+          .attr("src", stillGif)
+          .attr("data-jpeg_src", movingGif);
 
         let buttonContainer = $("<div>").addClass("button-container");
-        let downloadLink = $('<a download>').attr('target', '_blank').attr("href", gif.images.original.url);
-        let downloadButton = $('<button>').addClass('download-button').text("Download");
+        let downloadLink = $("<a download>")
+          .attr("target", "_blank")
+          .attr("href", gif.images.original.url);
+        let downloadButton = $("<button>")
+          .addClass("download-button")
+          .text("Download");
         let favoriteButton = $("<input type='submit' value='Favorite'>")
-          .addClass("favorite-button").attr("id", "favorite").attr('data-index', gifID)
-          .attr('data-rating', gifRating).attr('src', stillGif)
-          .attr('data-jpeg_src', movingGif);
+          .addClass("favorite-button")
+          .attr("id", "favorite")
+          .attr("data-index", gifID)
+          .attr("data-rating", gifRating)
+          .attr("src", stillGif)
+          .attr("data-jpeg_src", movingGif);
 
         $("#animals").append(gridItem);
         gridItem.append(imgDIV);
         downloadLink.append(downloadButton);
         buttonContainer.append(downloadLink).append(favoriteButton);
-        imgDIV.append(rating).append(gifIMG).append(buttonContainer);
+        imgDIV
+          .append(rating)
+          .append(gifIMG)
+          .append(buttonContainer);
       });
 
-      let faveGifs = JSON.parse(localStorage.getItem('faveGifs')) || [];
-      $(document).on('click', '#favorite', function () {
-        let id = $(this).data('index');
-        let rating = $(this).data('rating');
-        let stillGif = $(this).attr('src');
-        let movingGif = $(this).data('jpeg_src');
+      let faveGifs = JSON.parse(localStorage.getItem("faveGifs")) || [];
+      $(document).on("click", "#favorite", function() {
+        let id = $(this).data("index");
+        let rating = $(this).data("rating");
+        let stillGif = $(this).attr("src");
+        let movingGif = $(this).data("jpeg_src");
         let faveGif = {
           id,
           rating,
@@ -80,56 +94,65 @@ $(document).ready(() => {
           movingGif
         };
         faveGifs.push(faveGif);
-        localStorage.setItem('faveGifs', JSON.stringify(faveGifs));
+        localStorage.setItem("faveGifs", JSON.stringify(faveGifs));
         populateFaves();
       });
     });
   });
 
   function populateFaves() {
-    let faveGifs = JSON.parse(localStorage.getItem('faveGifs'));
+    let faveGifs = JSON.parse(localStorage.getItem("faveGifs"));
     $("#favoriteGifs").empty();
 
     $.map(faveGifs, faveGif => {
       let gifID = faveGif.id;
-      let rating = (faveGif.rating).toUpperCase();
+      let rating = faveGif.rating.toUpperCase();
       let stillGif = faveGif.stillGif;
       let movingGif = faveGif.movingGif;
 
-      let gridItem = $('<div>').addClass("grid-item");
+      let gridItem = $("<div>").addClass("grid-item");
       let faveDiv = $("<div>").addClass("fave-gif");
       let faveRating = $(`<p class='rating'>Rating: ${rating} </p>`);
-      let faveImg = $("<img alt='img'>").addClass("jpeg")
-        .attr('src', stillGif)
-        .attr('data-jpeg_src', movingGif);
+      let faveImg = $("<img alt='img'>")
+        .addClass("jpeg")
+        .attr("src", stillGif)
+        .attr("data-jpeg_src", movingGif);
 
       let buttonContainer = $("<div>").addClass("button-container");
-      let removeButton = $("<button>").addClass("remove-button")
-        .attr("id", "remove").attr('data-remove_index', gifID).text("Remove");
+      let removeButton = $("<button>")
+        .addClass("remove-button")
+        .attr("id", "remove")
+        .attr("data-remove_index", gifID)
+        .text("Remove");
 
       $("#favoriteGifs").append(gridItem);
       gridItem.append(faveDiv);
       buttonContainer.append(removeButton);
-      faveDiv.append(faveRating).append(faveGif).append(faveImg).append(buttonContainer);
+      faveDiv
+        .append(faveRating)
+        .append(faveGif)
+        .append(faveImg)
+        .append(buttonContainer);
     });
 
-    function removeAFavorite() {
-      $(document).on('click', '#remove', function () {
-        let removalKey = $(this).data('remove_index')
-        console.log(faveGifs);
-        //localStorage.remove(removalKey);
+      $(document).on("click", "#remove", function(e) {
+        e.preventDefault();
+        let gifsInStorage = JSON.parse(localStorage["faveGifs"]);
+        
+        $.map(gifsInStorage, gifToDelete => {
+          console.log(gifToDelete)
+        });
+        //localStorage.removeItem();
       });
-    }
-    removeAFavorite();
   }
   populateFaves();
 
-  $(document).on('mouseenter', '.jpeg', function () {
-    $(this).data('img_src', $(this).attr('src'));
-    $(this).attr('src', $(this).data('jpeg_src'));
+  $(document).on("mouseenter", ".jpeg", function() {
+    $(this).data("img_src", $(this).attr("src"));
+    $(this).attr("src", $(this).data("jpeg_src"));
   });
 
-  $(document).on('mouseleave', '.jpeg', function () {
-    $(this).attr('src', $(this).data('img_src'));
+  $(document).on("mouseleave", ".jpeg", function() {
+    $(this).attr("src", $(this).data("img_src"));
   });
 });
